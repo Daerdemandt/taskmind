c70dcd5aa81497332d7cc43c131752703f1c5a64
Roman Kuzmichev <daerdemandt@csgo.com>
2020-11-04 14:58:50 +0300

    Add example of rendering given tree

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,10 +3,31 @@ import ReactDOM from "react-dom";
 import React from "react";
 import { Mindmap } from 'remindjs';
 
+const onc = (...params) => console.log('onc!')
+
+const pow2 = (n) => n < 2 ? 0 : 1 + pow2(_.floor(n / 2));
+
+const nb = (n) => ({
+    id: _.uniqueId(),
+    title: `${n}`,
+    side: 'right',
+});
+
+const mkNode = (n) => {
+    const result = nb(n);
+    const p2 = pow2(n);
+    if (p2) {
+        result.children = {attached:[]};
+        result.children.attached.push(nb(Math.pow(2, p2)));
+        result.children.attached.push(mkNode(n - Math.pow(2, p2)));
+    }
+    return result;
+};
+
 const Main = () => (
     <div>
 		<h1>Hello World!</h1>
-        <Mindmap onChange={console.log} />
+        <Mindmap value={mkNode(10669)} onChange={onc} />
     </div>
 );
 
