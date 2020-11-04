65f278983915b79057f5d8ff1862cc0d66bcf542
Roman Kuzmichev <daerdemandt@csgo.com>
2020-11-04 15:52:47 +0300

    Add a component to draw trees

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,31 +3,44 @@ import ReactDOM from "react-dom";
 import React from "react";
 import { Mindmap } from 'remindjs';
 
-const onc = (...params) => console.log('onc!')
+const tree = [
+    {id: 1, parent:null, content:{title: 'this is root'}},
+    {id: 2, parent:1, content:{title: 'this has parent and child'}},
+    {id: 3, parent:1, content:{title: 'this is leaf'}},
+    {id: 4, parent:2, content:{title:'this is also leaf'}},
+];
 
-const pow2 = (n) => n < 2 ? 0 : 1 + pow2(_.floor(n / 2));
-
-const nb = (n) => ({
-    id: _.uniqueId(),
-    title: `${n}`,
-    side: 'right',
+const parseTree = (tree) => ({
+    nodeById: _.keyBy('id')(tree),
+    children: _.flow(_.groupBy('parent'), _.mapValues(_.map(_.prop('id'))))(tree),
+    rootId: tree.filter(_.isMatch({parent:null}))[0].id
 });
 
-const mkNode = (n) => {
-    const result = nb(n);
-    const p2 = pow2(n);
-    if (p2) {
-        result.children = {attached:[]};
-        result.children.attached.push(nb(Math.pow(2, p2)));
-        result.children.attached.push(mkNode(n - Math.pow(2, p2)));
-    }
-    return result;
+const mkMmNode = (node, renderTitle) => ({
+    id: node.id,
+    title: renderTitle(node),
+    side: 'right',
+    children: {attached:[]}
+});
+
+const treeRenderer = (renderTitle = _.prop('content.title')) => ({data}) => {
+    const {nodeById, children, rootId} = parseTree(data);
+    const buildMmTree = (id) => {
+        const result = mkMmNode(nodeById[id], renderTitle);
+        if (children[id]) {
+            result.children = {attached: children[id].map(buildMmTree)};
+        }
+        return result;
+    };
+    return <Mindmap value={buildMmTree(rootId)}/>;
 };
+
+const Tree = treeRenderer();
 
 const Main = () => (
     <div>
 		<h1>Hello World!</h1>
-        <Mindmap value={mkNode(10669)} onChange={onc} />
+        <Tree data={tree} />
     </div>
 );
 
